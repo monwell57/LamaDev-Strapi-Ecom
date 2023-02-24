a413b46023bfa48aaa2998a233d10b494c266108
Gregory Peters <gregorypeters@Gregorys-MBP.attlocal.net>
2023-02-24 13:43:07 -0800

    initial commit

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,61 @@
 import React from "react";
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import SearchIcon from "@mui/icons-material/Search";
 import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
 import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
 import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { Link } from "react-router-dom";
+import "./Navbar.scss" 
 
 const Navbar = () => {
   return (
     <div className="navbar">
       <div className="wrapper">
-        <div className="left"></div>
-        <div className="center"></div>
-        <div className="right"></div>
+        <div className="left">
+          <div className="item">
+            <img src="/img/en.png" width={30} height={18} alt="" />
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <span>USD</span>
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <Link to="/products/1">Women</Link>
+          </div>
+          <div className="item">
+            <Link to="/products/2">Men</Link>
+          </div>
+          <div className="item">
+            <Link to="/products/3">Children</Link>
+          </div>
+        </div>
+        <div className="center">
+          <Link to="/">STREETSTORE</Link>
+        </div>
+        <div className="right">
+          <div className="center">
+            <Link to="/">Homepage</Link>
+          </div>
+          <div className="center">
+            <Link to="/">About</Link>
+          </div>
+          <div className="center">
+            <Link to="/">Contact</Link>
+          </div>
+          <div className="center">
+            <Link to="/">Stores</Link>
+          </div>
+          <div className="icons">
+            <SearchIcon />
+            <PersonOutlineOutlinedIcon />
+            <FavoriteBorderOutlinedIcon />
+            <div className="cartIcon">
+              <ShoppingCartOutlinedIcon />
+              <span>0</span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
